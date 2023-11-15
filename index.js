54ad4ce963d678bec51744911355001e7866b16e
Delaney Reed <DelaneyReed2001@Gmail.com>
2023-11-15 11:16:07 -0700

    added express to index

--- index.js
+++ index.js
@@ -1,24 +1,40 @@
+const express = require('express');
+const app = express();
+
+// The service port. In production the front-end code is statically hosted by the service on the same port.
+const port = process.argv.length > 2 ? process.argv[2] : 3000;
+
+// JSON body parsing using built-in middleware
+app.use(express.json());
+
+// Serve up the front-end static content hosting
+app.use(express.static('public'));
+
+// Router for service endpoints
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
+
+
+// Get the elements for the welcome message, name input, and login button
 const welcomeMessageElement = document.getElementById('welcome-message');
 const nameInput = document.getElementById('name');
 const loginButton = document.getElementById('login-button');
 
+
+// Add an event listener to the login button
 loginButton.addEventListener('click', function (event) {
-  event.preventDefault();
+ event.preventDefault(); // Prevent the form from submitting
 
-  const enteredName = nameInput.value;
 
-  fetch('/api/updateName', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ name: enteredName }),
-  })
-    .then(response => response.json())
-    .then(data => {
-      welcomeMessageElement.textContent = data.message;
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
+ const enteredName = nameInput.value;
+
+
+ // Check if a name is entered
+ if (enteredName.trim() !== '') {
+   // Display the personalized welcome message
+   welcomeMessageElement.textContent = `Welcome, ${enteredName}!`;
+ } else {
+   // If no name is entered, show a generic welcome message
+   welcomeMessageElement.textContent = 'Welcome!';
+ }
 });
