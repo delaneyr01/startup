3331ce971f57ae0d9dc0fa92eada8bb5c1a9f48d
Delaney Reed <DelaneyReed2001@Gmail.com>
2023-11-15 11:25:17 -0700

    created client.js to handle the service

---
+++ client.js
@@ -0,0 +1,20 @@
+// client.js
+const welcomeMessageElement = document.getElementById('welcome-message');
+const nameInput = document.getElementById('name');
+const loginButton = document.getElementById('login-button');
+
+// Add an event listener to the login button
+loginButton.addEventListener('click', function (event) {
+  event.preventDefault(); // Prevent the form from submitting
+
+  const enteredName = nameInput.value;
+
+  // Check if a name is entered
+  if (enteredName.trim() !== '') {
+    // Display the personalized welcome message
+    welcomeMessageElement.textContent = `Welcome, ${enteredName}!`;
+  } else {
+    // If no name is entered, show a generic welcome message
+    welcomeMessageElement.textContent = 'Welcome!';
+  }
+});
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
+// index.js
 const express = require('express');
 const app = express();
-
-// The service port. In production the front-end code is statically hosted by the service on the same port.
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
 // JSON body parsing using built-in middleware
@@ -11,30 +10,10 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // Router for service endpoints
-var apiRouter = express.Router();
+const apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
-
-// Get the elements for the welcome message, name input, and login button
-const welcomeMessageElement = document.getElementById('welcome-message');
-const nameInput = document.getElementById('name');
-const loginButton = document.getElementById('login-button');
-
-
-// Add an event listener to the login button
-loginButton.addEventListener('click', function (event) {
- event.preventDefault(); // Prevent the form from submitting
-
-
- const enteredName = nameInput.value;
-
-
- // Check if a name is entered
- if (enteredName.trim() !== '') {
-   // Display the personalized welcome message
-   welcomeMessageElement.textContent = `Welcome, ${enteredName}!`;
- } else {
-   // If no name is entered, show a generic welcome message
-   welcomeMessageElement.textContent = 'Welcome!';
- }
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
